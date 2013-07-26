fdd40bf269ff9aa61edd2c34f2769fd80c98b2bd
bendem <bendembd@gmail.com>
2013-07-26 14:41:53 +0200

    First commit

---
+++ src/be/bendem/OnDeathCoord/OnDeathCoord.java
@@ -0,0 +1,31 @@
+package be.bendem.OnDeathCoord;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * OnDeathCoord for Bukkit
+ *
+ * @author bendem
+ */
+public class OnDeathCoord extends JavaPlugin {
+    
+    /**
+     *
+     */
+    @Override
+    public void onEnable() {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+        
+        getServer().getPluginManager().registerEvents(new DeathListener(), this);
+    }
+    /**
+     *
+     */
+    @Override
+    public void onDisable() {
+        getLogger().info("Goodbye world!");
+    }
+}
+
