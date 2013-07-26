9c730e7ef8d1379310171142aea0ecc81a80fc44
bendem <bendembd@gmail.com>
2013-07-26 16:20:59 +0200

    Switch to maven

---
+++ src/main/java/be/bendem/OnDeathCoord/DeathListener.java
@@ -0,0 +1,18 @@
+package be.bendem.OnDeathCoord;
+
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+
+	@EventHandler
+	public void death(PlayerDeathEvent event) {
+		Location playerLocation = event.getEntity().getLocation();
+		event.setDeathMessage(event.getDeathMessage() + " :  "
+				+ Math.round(playerLocation.getX()) + " / "
+				+ Math.round(playerLocation.getY()) + " / "
+				+ Math.round(playerLocation.getZ()));
+	}
+}
