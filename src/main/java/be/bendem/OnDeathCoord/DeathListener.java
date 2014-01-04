b282f3ba54bdc7b430f4479bbefe1841071aa977
bendem <bendembd@gmail.com>
2014-01-04 12:31:07 +0100

    Added colors and despawn time message
    + Added private despawn time message
    + Added colors to the death messages and despawn messages

--- src/main/java/be/bendem/OnDeathCoord/DeathListener.java
+++ src/main/java/be/bendem/OnDeathCoord/DeathListener.java
@@ -1,11 +1,15 @@
 package be.bendem.OnDeathCoord;
 
-import org.bukkit.ChatColor;
+import be.bendem.common.ChatFormatter;
+
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class DeathListener implements Listener {
 
@@ -13,36 +17,46 @@ public class DeathListener implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player   player = event.getEntity();
         Location playerLocation = player.getLocation();
-        String   deathMsg, coordinates;
+        String   deathMessage, coordinates, playerMessage = new String();
 
-        // TODO Adding colors
-        // player
-        // ChatColor.COLOR_CHAR + ChatColor.BOLD
-        // message
-        // ChatColor.COLOR_CHAR + ChatColor.RESET
-        // ChatColor.COLOR_CHAR + ChatColor.DARK_GREY
-        // coordinates
-        // ChatColor.COLOR_CHAR + ChatColor.BOLD
+        coordinates = ChatFormatter.darkGray(
+              Math.round(playerLocation.getX()) + ", "
+            + Math.round(playerLocation.getY()) + ", "
+            + Math.round(playerLocation.getZ())
+        );
 
-        coordinates = Math.round(playerLocation.getX()) + " - "
-                    + Math.round(playerLocation.getY()) + " - "
-                    + Math.round(playerLocation.getZ());
-
-        if(OnDeathCoord.config.getBoolean("public")) {
-            deathMsg = OnDeathCoord.config.getString(
-                "public-messages." + player.getLastDamageCause().getCause().name().toLowerCase(),
+        if(OnDeathCoord.config.getBoolean("public-death")) {
+            deathMessage = OnDeathCoord.config.getString(
+                "public-death-messages." + player.getLastDamageCause().getCause().name().toLowerCase(),
                 "%s died at %s"
             );
-            deathMsg = String.format(deathMsg, player.getDisplayName(), coordinates);
-            OnDeathCoord.logger.info("Death message set to '" + deathMsg + "'");
-            event.setDeathMessage(deathMsg);
+            // injecting informations
+            deathMessage = String.format(deathMessage, ChatFormatter.darkGreen(player.getDisplayName()), coordinates);
+            event.setDeathMessage(deathMessage);
         } else {
-            deathMsg = OnDeathCoord.config.getString("private-message", "%s died at %s");
-            deathMsg = String.format(deathMsg, coordinates);
-            player.sendMessage(deathMsg);
+            deathMessage = OnDeathCoord.config.getString(
+                "private-death-message",
+                ChatFormatter.bold("You") + ChatFormatter.darkGray(" died at ") + "%s"
+            );
+            // injecting informations
+            deathMessage = String.format(deathMessage, coordinates);
+            playerMessage = deathMessage + " ";
         }
 
+        if(OnDeathCoord.config.getBoolean("show-despawn-time") && event.getDrops().size() != 0) {
+            playerMessage += OnDeathCoord.config.getString("despawn-message", "Your items will despawn at %s");
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.SECOND, OnDeathCoord.config.getInt("despawn-time", 300));
 
-        OnDeathCoord.logger.info(deathMsg);
+            // injection
+            playerMessage = String.format(playerMessage, ChatFormatter.red(sdf.format(cal.getTime())));
+        }
+        player.sendMessage(playerMessage);
+
+        if(OnDeathCoord.config.getBoolean("remind-at-despawn")) {
+            // TODO Ajout du schedule de despawn message
+            // http://jd.bukkit.org/beta/apidocs/index.html?org/bukkit/scheduler/BukkitScheduler.html
+        }
     }
 }
